faa0978d06266cc679f3297aeaa77331529e7e95
idkqh7 <idkqh7@gmail.com>
2014-02-17 04:59:01 +0900

    add iPython
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    # On branch master
    # Your branch is ahead of 'origin/master' by 2 commits.
    #   (use "git push" to publish your local commits)
    #
    # Changes to be committed:
    #	modified:   app/views/index.scala.html
    #	modified:   app/views/main.scala.html
    #	modified:   app/views/pages/links.scala.html
    #

--- app/controllers/MainController.scala
+++ app/controllers/MainController.scala
@@ -5,16 +5,16 @@ import play.api.mvc.{Action, Controller}
 
 object MainController extends Controller {
 
-  case class Contents(id: String, name:String)
-  implicit def set(combined: (String, String)): Contents = combined match {
-    case (id, name) => Contents(id, name)
+  case class Contents(id: String, name:String, url:String)
+  implicit def set(combined: (String, String, String)): Contents = combined match {
+    case (id, name, url) => Contents(id, name, url)
   }
 
   val contents: Array[Contents]  = Array(
-    ("redmine", "Redmine"),
-    ("jenkins", "Jenkins"),
-    ("ipython", "iPython"),
-    ("owncloud", "OwnCloud")
+    ("redmine", "Redmine" ,""),
+    ("jenkins", "Jenkins", ""),
+    ("ipython", "iPython", "ipython/notebooks/console.ipynb"),
+    ("owncloud", "OwnCloud", "")
   )
   def index() = Action {
     Ok(views.html.index(contents))
